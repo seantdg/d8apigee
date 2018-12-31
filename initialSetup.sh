5aae3eccd2e341a6be93d2d6ef430ea8a0cfefbc
Laughing Biscuit <laughingbiscuit@example.com>
2018-12-31 17:14:59 +0000

    permissions test

--- initialSetup.sh
+++ initialSetup.sh
@@ -10,6 +10,7 @@ docker exec -i some-drupal chmod -R 777 themes
 docker exec -i some-drupal chmod -R 777 sites
 docker exec -i some-drupal chmod -R 777 profiles
 docker exec -i some-drupal chmod -R 777 modules
+docker exec -i some-drupal chmod -R 777 core
 docker exec -i some-drupal ./vendor/drush/drush/drush en -y apigee_edge swagger_ui_formatter apigee_edge_apiproduct_rbac features features_ui api_specification spec_publish
 docker exec -i some-drupal ./vendor/drush/drush/drush key:save apigee_edge_connection_default --key-type=apigee_auth --key-provider=apigee_edge_environment_variables
 docker exec -i some-drupal ./vendor/drush/drush/drush apigee-edge:sync
